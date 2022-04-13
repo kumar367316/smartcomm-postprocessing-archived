fd11824ccee80a1a6a8de50435f684ff41387f8d
kumar <85148531+kumar367316@users.noreply.github.com>
2022-04-13 17:52:12 +0530

    Update PostProcessingController.java

--- src/main/java/com/htc/postprocessing/controller/PostProcessingController.java
+++ src/main/java/com/htc/postprocessing/controller/PostProcessingController.java
@@ -1,6 +1,6 @@
 package com.htc.postprocessing.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
