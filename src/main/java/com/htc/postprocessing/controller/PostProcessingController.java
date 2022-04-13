65bbdb0caa7fabfd2160dc8b4ab1a66dc7e21569
kumar <85148531+kumar367316@users.noreply.github.com>
2022-04-13 17:51:29 +0530

    get mapping

--- src/main/java/com/htc/postprocessing/controller/PostProcessingController.java
+++ src/main/java/com/htc/postprocessing/controller/PostProcessingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostProcessingController {
 
-	@PostMapping(path = "/message")
+	@GetMapping(path = "/message")
 	public String message() {
 		return "postprocessing";
 	}
